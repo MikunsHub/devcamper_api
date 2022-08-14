d8011200556e820b981fd9d83c0df9acc1fde8bb
MikunsHub <63354714+MikunsHub@users.noreply.github.com>
2022-08-14 21:46:38 +0100

    connected the application to mongodb

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected to ${conn.connection.host}`.cyan.underline.bold);
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 //load env vars
 dotenv.config({path: './config/config.env'});
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express();
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+// app.use(logger)
+
+//MOunt routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-    );
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+})
